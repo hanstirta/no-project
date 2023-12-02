078f7c290a2307d2b7371d6a367064559ae93718
HansTirta <80604030+hanstirta@users.noreply.github.com>
2023-12-02 21:56:00 +0700

    Add files via upload

---
+++ 001_WPU_Dasar Pemrograman dengan Javascript/014_ARRAY pada JAVASCRIPT Manipulasi ARRAY/script.js
@@ -0,0 +1,64 @@
+// manipulasi array
+
+// 1. menambah isi array
+// let arr = [];
+// arr[0] = 'Sandhika';
+// arr[1] = 'Galih';
+// arr[2] = 'Nofa';
+// arr[3] = 'Doddy';
+// console.log(arr);
+
+
+// 2. Menghapus isi array
+// let arr = ['Sandhika', 'Galih', 'Nova'];
+// arr[1] = undefined;
+// console.log(arr);
+
+
+// 3. Menampilkan isi array
+// let arr = ['Sandhika', 'Galih', 'Nova'];
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// method pada array
+// 1. join
+// let arr = ['Sandhika', 'Galih', 'Nova'];
+// console.log(arr.join(' - '));
+
+
+// 2. push & pop
+// arr.push('Doddy');
+// arr.pop();
+// console.log(arr.join(' - '));
+
+// 3. unshift & shift
+// arr.unshift('Doddy');
+// arr.shift();
+// console.log(arr.join(' - '));
+
