677a782302b6691edd8748af035742317a8bdfcb
HansTirta <80604030+hanstirta@users.noreply.github.com>
2023-12-02 21:53:43 +0700

    Add files via upload

---
+++ 001_WPU_Dasar Pemrograman dengan Javascript/013_FUNCTION pada JAVASCRIPT DECLARATION vs EXPRESSION/script.js
@@ -0,0 +1,14 @@
+// function declaration
+// function tampilPesan (nama) {
+//     alert ('Halo ' + nama);
+// }
+// tampilPesan('Hans Tirta');
+
+
+
+// function expression
+// disimpan dalam sebuah variabel
+// let tampilPesan = function (nama) {
+//     alert('Halo ' + nama);
+// }
+// tampilPesan('Hans');
