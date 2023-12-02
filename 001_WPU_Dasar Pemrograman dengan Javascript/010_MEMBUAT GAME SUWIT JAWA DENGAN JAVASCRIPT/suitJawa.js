cdcff701067b072cba997f77ef8368881e603e95
HansTirta <80604030+hanstirta@users.noreply.github.com>
2023-12-02 19:23:30 +0700

    Add files via upload

---
+++ 001_WPU_Dasar Pemrograman dengan Javascript/010_MEMBUAT GAME SUWIT JAWA DENGAN JAVASCRIPT/suitJawa.js
@@ -0,0 +1,55 @@
+let tanya = true;
+while ( tanya ){
+
+    // menangkap pilihan player
+    let p = prompt('pilih : gajah, semut, orang');
+
+    // menangkap pilihan komputer
+    // menangkap plihan 
+    let comp = Math.random();
+
+
+    if(comp < 0.34 ){
+        comp = 'gajah';
+    } else if( comp >= 0.34 && comp < 0.67) {
+        comp = 'orang';
+    } else {
+        comp = 'semut';
+    }
+        
+
+    let hasil = '';
+    // menentukan rules
+    if( p == comp ){
+        hasil = 'SERI!';
+    } else if ( p == 'gajah'){
+        // if( comp == 'orang'){
+        //     hasil = 'MENANG';
+        // } else {
+        //     hasil = 'KALAH';
+        // }
+
+        hasil = ( comp = 'orang' ) ? 'MENANG!' : 'KALAH!';
+    } else if( p == 'orang'){
+        // if( comp == 'gajah'){
+        //     hasil = 'KALAH';
+        // } else {
+        //     hasil = 'MENANG;'
+        // }
+        hasil = ( comp = 'gajah') ? 'KALAH!' : 'MENANG!';
+    } else if ( p == 'semut'){
+
+        hasil = (comp == 'orang') ? 'KALAH!' : 'MENANG!';
+    } else {
+        hasil = ' memasukkan pilihan yang salah!!!'
+    }
+
+
+    // tampilkan hasilnya
+    alert('Kamu memilih ' + p + ' dan Komputer memilih : ' + comp + '\nMaka hasilnya : Kamu ' + hasil);
+
+
+    tanya = confirm('Main Lagi?');
+}
+
+alert ('Terimakasih Sudah Bermain.')
